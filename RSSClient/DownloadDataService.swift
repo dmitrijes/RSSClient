0efce57e43f653bb604c5eee3da1389c3aa804b0
Dima <dzmitry_fralou@epbygrow0352.grodno.epam.com>
2017-05-10 14:52:04 +0300

    Download Data

---
+++ RSSClient/DownloadDataService.swift
@@ -0,0 +1,40 @@
+//
+//  File.swift
+//  RSSClient
+//
+//  Created by Dzmitry Fralou on 10.05.17.
+//  Copyright © 2017 Dzmitry Fralou. All rights reserved.
+//
+
+import Foundation
+
+
+class DownloadDataService {
+    
+    let url = "http://feeds.macrumors.com/MacRumors-All"
+    
+    
+    func downloadData() {
+        let queue = DispatchQueue.global(qos: .userInteractive)
+        queue.async {
+            guard let newUrl = URL(string: self.url) else {
+                print("Can't URL")
+                return
+            }
+            let urlReq = URLRequest(url: newUrl)
+            
+            let config = URLSessionConfiguration.default
+            let session = URLSession(configuration: config)
+            
+            let task = session.dataTask(with: urlReq) { (data, response, error) in
+                print(data ?? "not")
+                print("------")
+                print(response ?? "not")
+                print("------")
+                print(error ?? "not")
+            }
+            task.resume()
+        }
+    }
+    
+}
--- RSSClient/ViewController.swift
+++ RSSClient/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let downloadData = DownloadDataService()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        downloadData.downloadData()
     }
 
 
