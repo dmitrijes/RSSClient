b93b4c88ecf86e95471cccbec427f3360cc60a96
Dima <dzmitry_fralou@epbygrow0352.grodno.epam.com>
2017-08-03 15:41:34 +0300

    Detail Date

--- RSSClient/DetailViewController.swift
+++ RSSClient/DetailViewController.swift
@@ -29,7 +29,7 @@ extension DetailViewController: DetailViewDataSource {
     }
     
     func getDate() -> String {
-        return post.postDate ?? ""
+        return DateFormat().getDatePost(date: post.postDate ?? "")
     }
     
     func getImage() -> UIImage? {
